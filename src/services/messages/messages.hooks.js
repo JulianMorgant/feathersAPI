8a825554014e5d42b70d2b698d35f6e03622bb99
Julian <julian.morgant@gmail.com>
2019-12-13 14:29:42 +0100

    feat: add role based security

--- src/services/messages/messages.hooks.js
+++ src/services/messages/messages.hooks.js
@@ -1,28 +1,28 @@
 
 const { authenticate } = require('@feathersjs/authentication').hooks;
-// const { Forbidden } = require('feathers-errors');
+const { Forbidden } = require('@feathersjs/errors');
 
 const globalAuditingCreate = require('../../hooks/global-auditing-create');
 const globalAuditingUpdate = require('../../hooks/global-auditing-update');
 
-/*
+
 function hasRole(name) {
   return function(context) {
     const { user = {} } = context.params;
     if(!user.roles || !user.roles.split(',').includes(name)) {
       throw new Forbidden('You are not allowed to access this');
+      // throw new Forbidden('You are not allowed to access this');
     }
   }
 }
-*/
+
 
 module.exports = {
   before: {
     all: [ authenticate('jwt') ],
     find: [],
     get: [],
-//    create: [globalAuditingCreate(),hasRole('ADMIN')],
-    create: [globalAuditingCreate()],
+    create: [globalAuditingCreate(),hasRole('ADMIN')],
     update: [globalAuditingUpdate()],
     patch: [globalAuditingUpdate()],
     remove: []
